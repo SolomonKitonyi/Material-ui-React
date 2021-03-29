bf8a429324122a1b9e777ac81b18ecd98b962a93
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-29 16:23:49 +0300

    add text field

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
+import TextField from '@material-ui/core/TextField';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TextField />
         <ButtonGroup size="large" variant="contained">
             <Button startIcon={<SaveIcon/>} color="primary">
               Save
