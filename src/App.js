a23500a5d5a4f1c5e7b693ef4d8a4be68c146097
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-29 16:53:15 +0300

    add container

--- src/App.js
+++ src/App.js
@@ -5,24 +5,27 @@ import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
 import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TextField variant="filled" color="green" type="date" label="The date" />
-        <br/> <br/>
-        <ButtonGroup size="large" variant="contained">
-            <Button startIcon={<SaveIcon/>} color="primary">
-              Save
-            </Button> 
-            <Button startIcon={<DeleteIcon/>} color="secondary">
-              Discard
-            </Button> 
-        </ButtonGroup>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Container maxWidth='sm'>
+      <div className="App">
+        <header className="App-header">
+          <TextField variant="filled" color="green" type="date" label="The date" />
+          <br/> <br/>
+          <ButtonGroup size="large" variant="contained">
+              <Button startIcon={<SaveIcon/>} color="primary">
+                Save
+              </Button> 
+              <Button startIcon={<DeleteIcon/>} color="secondary">
+                Discard
+              </Button> 
+          </ButtonGroup>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    </Container>
   );
 }
 
